18eacd3c11811c4270ef88c8876e4b4bcd923963
songihyeon <tyumr70@gmail.com>
2025-04-08 16:00:24 +0900

    task completed

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,1 +1,10 @@
-contract MyToken {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("CoJinNam", "CJN") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,9 @@ const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     ganache: {
-      url: '', // Todo: Ganache RPC URL
+      url: 'HTTP://127.0.0.1:7545', // Todo: Ganache RPC URL
       accounts: [
-        process.env.PRIVATE_KEY || '', // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
+        process.env.PRIVATE_KEY || '0x97ecbce67a9a2624e52f99de30a6959ba6a043f5dab4d58926eb0599f7ff5737', // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
       ],
     },
   },
